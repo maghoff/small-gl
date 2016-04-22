24d5cf9704ac0eb123e4be24c0244665b755a146
Magnus Hoff <maghoff@gmail.com>
2016-04-22 22:53:45 +0200

    Split out EGL bits

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-SRCS=main.c gl_bits.c x11_bits.c
+SRCS=main.c gl_bits.c x11_bits.c egl_bits.c
 
 main: $(SRCS)
 	gcc -Wall -o $@ $(SRCS) -lX11 -lEGL -lGLESv2 -lm -Os -flto -s
---
+++ egl_bits.c
@@ -0,0 +1,57 @@
+#include <EGL/egl.h>
+
+static EGLDisplay egl_display;
+static EGLContext egl_context;
+static EGLSurface egl_surface;
+
+int init_egl(EGLNativeDisplayType display, EGLNativeWindowType win) {
+	egl_display = eglGetDisplay(display);
+	if ( egl_display == EGL_NO_DISPLAY ) {
+		return 1;
+	}
+
+	if ( !eglInitialize( egl_display, NULL, NULL ) ) {
+		return 1;
+	}
+
+	EGLint attr[] = {       // some attributes to set up our egl-interface
+		EGL_BUFFER_SIZE, 16,
+		EGL_RENDERABLE_TYPE,
+		EGL_OPENGL_ES2_BIT,
+		EGL_NONE
+	};
+
+	EGLConfig  ecfg;
+	EGLint     num_config;
+	if ( !eglChooseConfig( egl_display, attr, &ecfg, 1, &num_config ) ) {
+		return 1;
+	}
+
+	if ( num_config != 1 ) {
+		return 1;
+	}
+
+	egl_surface = eglCreateWindowSurface(egl_display, ecfg, win, NULL);
+	if ( egl_surface == EGL_NO_SURFACE ) {
+		return 1;
+	}
+
+	//// egl-contexts collect all state descriptions needed required for operation
+	EGLint ctxattr[] = {
+		EGL_CONTEXT_CLIENT_VERSION, 2,
+		EGL_NONE
+	};
+	egl_context = eglCreateContext ( egl_display, ecfg, EGL_NO_CONTEXT, ctxattr );
+	if ( egl_context == EGL_NO_CONTEXT ) {
+		return 1;
+	}
+
+	//// associate the egl-context with the egl-surface
+	eglMakeCurrent( egl_display, egl_surface, egl_surface, egl_context );
+
+	return 0;
+}
+
+void flip() {
+	eglSwapBuffers(egl_display, egl_surface);
+}
