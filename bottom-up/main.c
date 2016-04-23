cf88e11b6c72065cd34bfa176e34e014b9d11c60
Magnus Hoff <maghoff@gmail.com>
2016-04-23 12:33:57 +0200

    Successfully build without stdlib

--- bottom-up/main.c
+++ bottom-up/main.c
@@ -1,3 +1,15 @@
+#include <sys/syscall.h>
+
+void my_exit(void) {
+    __asm__ volatile (
+        "xorq %%rdi, %%rdi\n"
+        "movq %0, %%rax\n"
+        "syscall\n"
+        :
+        : "id"(SYS_exit));
+	__builtin_unreachable();
+}
+
 void _start() {
-	for (;;) {}
+	my_exit();
 }
