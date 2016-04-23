056e06f05009ea84396f8265cdf401171aaa0254
Magnus Hoff <maghoff@gmail.com>
2016-04-23 12:45:28 +0200

    Whitespace

--- bottom-up/main.c
+++ bottom-up/main.c
@@ -1,12 +1,12 @@
 #include <sys/syscall.h>
 
 void my_exit(void) {
-    __asm__ volatile (
-        "xorq %%rdi, %%rdi\n"
-        "movq %0, %%rax\n"
-        "syscall\n"
-        :
-        : "id"(SYS_exit));
+	__asm__ volatile (
+		"xorq %%rdi, %%rdi\n"
+		"movq %0, %%rax\n"
+		"syscall\n"
+		:
+		: "id"(SYS_exit));
 	__builtin_unreachable();
 }
 
